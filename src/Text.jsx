c954a794f11a45211033ec05e268ff5f7f368124
katmart93 <katwlodarczyk93@gmail.com>
2022-08-14 18:10:13 +0200

    text updated

--- src/Text.jsx
+++ src/Text.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function Text() {
   return (
     <div className="text">
-      Enter your text below, choose the case you want to convert it to, font
-      style and font size.
+      Enter your text below, choose font style, font size and the case you want
+      to convert it to.
     </div>
   );
 }
