3013de478cb768cc019dfaffe9636df00b8abdd6
katmart93 <katwlodarczyk93@gmail.com>
2022-08-15 12:47:25 +0200

    opening github page in a new tab

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons";
 export default function Footer() {
   return (
     <div className="footer">
-      <a href="https://github.com/katmart93">
+      <a href="https://github.com/katmart93" target="_blank">
         <FontAwesomeIcon icon={faGithub} />
       </a>
     </div>
