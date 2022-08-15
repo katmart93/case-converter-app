af5e2bee1c055b79b707ccc39282861fb19833e9
katmart93 <katwlodarczyk93@gmail.com>
2022-08-15 12:44:52 +0200

    github icon added to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,12 @@
-import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 export default function Footer() {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      <a href="https://github.com/katmart93">
+        <FontAwesomeIcon icon={faGithub} />
+      </a>
+    </div>
+  );
 }
